2703bc7777ef4f088661d1d472a65c374c7d2ca5
heroku-miraheze <61781343+heroku-miraheze@users.noreply.github.com>
2020-06-30 18:36:54 +0200

    Create desactivar.php

---
+++ desactivar.php
@@ -0,0 +1,5 @@
+<?php
+$mensaje = "Hola, en cuánto se borre Entretenimiento se parará de revertir. Paras en cuánto se borre Entretenimiento. Saludos."
+mail('joaquinito2051@gmail.com', 'Desactivación del bot', $mensaje);
+echo "Gracias, en cuánto se borre Entretenimiento se parará de revertir.";
+?>
